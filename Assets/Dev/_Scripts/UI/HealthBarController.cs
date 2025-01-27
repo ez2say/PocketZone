08d18795068e874593c2d237b7e9d08b6f76a2b6
Nikita <137696366+ez2say@users.noreply.github.com>
2025-01-27 10:51:11 +0300

    fix: HealthBar

--- Assets/Dev/_Scripts/UI/HealthBarController.cs
+++ Assets/Dev/_Scripts/UI/HealthBarController.cs
@@ -7,19 +7,12 @@ namespace PocketZone
         [SerializeField] private Transform _target;
         [SerializeField] private Vector3 _offset;
 
+        
+
         public void SetTarget(Transform target, Vector3 offset)
         {
             _target = target;
             _offset = offset;
-        }
-
-        void LateUpdate()
-        {
-            if (_target != null)
-            {
-                transform.position = _target.position + _offset;
-                transform.rotation = Camera.main.transform.rotation;
-            }
         }
     }
 }
